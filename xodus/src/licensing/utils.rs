3bc9574a686b8b418aa84619c42cc7a15ee924c2
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-06-11 11:31:37 +0200

    utils: simplify generate_string

--- xodus/src/licensing/utils.rs
+++ xodus/src/licensing/utils.rs
@@ -1,13 +1,9 @@
-use rand::RngExt;
+use rand::distr::{Alphanumeric, SampleString};
 
 pub fn generate_suid() -> String {
     "S-1-5-21-0000000000-0000000000-0000000000-1001".to_string()
 }
 
 pub fn generate_string(length: usize) -> String {
-    rand::rng()
-        .sample_iter(rand::distr::Alphanumeric)
-        .take(length)
-        .map(char::from)
-        .collect()
+    Alphanumeric.sample_string(&mut rand::rng(), length)
 }
