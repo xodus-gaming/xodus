b5c7454ec7ae812b1705357c4bbb3707b2fbff95
Christopher Homberger <christopher.homberger@web.de>
2026-06-13 15:15:17 +0200

    finish extract command

--- xodus-cli/src/commands/extract.rs
+++ xodus-cli/src/commands/extract.rs
@@ -1,4 +1,104 @@
-use xodus::xvd::utils::parse_file;
-pub async fn run(path: String) {
-    parse_file(path).await.expect("Failed to parse");
+use xodus::{licensing::splicense::{derive_device_key, parse_license, unpack_key}, models::{live::ExchangeUserTokenOutcome, secrets::Token, soap}, xvd::utils::{parse_file, unpack_file}};
+
+use crate::{device, user};
+pub async fn run(client: &reqwest::Client, path: String, destination: String, content_id: String, market: String) {
+    let dev_token = device::get_device_token().unwrap();
+    let Token::Legacy(dev_token) = dev_token else {
+        eprintln!("Invalid STS token");
+        return;
+    };
+    let user = user::get_user().unwrap();
+    let user_token = user::get_token("http://Passport.NET/STS".to_string()).unwrap();
+    let Token::Legacy(legacy) = user_token else {
+        eprintln!("Unspported user token");
+        return;
+    };
+
+    let secret = dev_token.binary_secret.unwrap();
+
+    let ms_device_token = xodus::api::live::exchange_device_token(
+        client,
+        dev_token.token.clone(),
+        secret.clone(),
+        "{d6d5a677-0872-4ab0-9442-bb792fce85c5}".to_string(),
+        "www.microsoft.com".to_owned(),
+        Some(soap::PolicyReference::mbi_ssl()),
+    )
+    .await
+    .unwrap();
+
+    let user_token = xodus::api::live::exchange_user_token(
+        client,
+        legacy.token,
+        user.username,
+        dev_token.token,
+        secret,
+        None,
+        Some("Silent".to_string()),
+        "{d6d5a677-0872-4ab0-9442-bb792fce85c5}".to_string(),
+        &[(
+            "www.microsoft.com".to_owned(),
+            Some(soap::PolicyReference::mbi_ssl()),
+        )],
+    )
+    .await
+    .expect("Failed to get ms user token");
+
+    let ms_device_token: Token = ms_device_token.into();
+    let Token::Compact(ms_device_token) = ms_device_token else {
+        eprintln!("Unsupported token");
+        return;
+    };
+
+    let user_token: Token = match user_token {
+        ExchangeUserTokenOutcome::Fault(_) => {
+            eprintln!("Failed to get exchange MS token");
+            return;
+        }
+        ExchangeUserTokenOutcome::Issued(
+            soap::BodyContent::RequestSecurityTokenResponseCollection(mut collection),
+        ) => {
+            let token = collection.security_tokens.remove(0);
+            token.into()
+        }
+        ExchangeUserTokenOutcome::Issued(soap::BodyContent::RequestSecurityTokenResponse(
+            token,
+        )) => token.into(),
+        _ => unreachable!("Only responses are handled"),
+    };
+    let Token::Compact(user_token) = user_token else {
+        eprintln!("Unsupported token");
+        return;
+    };
+
+    let (_response, game_license) = xodus::licensing::content::get_license_content(
+        client,
+        ms_device_token,
+        user_token,
+        user.puid,
+        content_id,
+        market,
+    )
+    .await
+    .expect("failed to get license");
+
+    let game_splicense = parse_license(game_license.splicense_block);
+
+    let dev_license = device::get_dev_license().unwrap();
+    let device_license = parse_license(dev_license.splicense);
+    let key = derive_device_key(&device_license.encrypted_device_key);
+    let key: [u8; 16] = key.try_into().expect("Key too big");
+    
+    for (uuid, content_key) in game_splicense.content_keys {
+        let unpacked: Vec<u8> = unpack_key(&key, content_key).expect("failed to unpack");
+        let xvd = parse_file(path.to_string()).await.expect("Failed to parse");
+        unpack_file(
+            xvd,
+            path.to_string(),
+            destination.to_string(),
+            unpacked.try_into().expect("match"),
+        )
+        .expect("unpack ok");
+        return;
+    }
 }
--- xodus-cli/src/main.rs
+++ xodus-cli/src/main.rs
@@ -23,7 +23,12 @@ enum SubCommand {
         market: Option<String>,
     },
     Extract {
+        #[clap(help = "Content Id of a license")]
+        content_id: String,
         path: String,
+        destination: String,
+        #[arg(short, long)]
+        market: Option<String>,
     },
     Login,
 }
@@ -70,8 +75,13 @@ async fn main() {
         SubCommand::Login => {
             commands::login::run(&client).await;
         }
-        SubCommand::Extract { path } => {
-            commands::extract::run(path).await;
+        SubCommand::Extract {
+            path,
+            destination,
+            content_id,
+            market
+        } => {
+            commands::extract::run(&client, path, destination, content_id, market.unwrap_or("neutral".to_string())).await;
         }
     }
 
