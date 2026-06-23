9d1913d59547829d6cef603dec94f8e08c7c5af2
Paweł Lidwin <lidwinpawel@gmail.com>
2026-06-23 16:42:47 +0200

    feat: add allow ui msa token request flag

--- xodus/src/models/xgameruntime/xuser.rs
+++ xodus/src/models/xgameruntime/xuser.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "PascalCase")]
 pub struct MSATokenRequest {
     pub client_id: String,
+    #[serde(default)]
+    pub allow_ui: bool
 }
 
 #[derive(Serialize)]
