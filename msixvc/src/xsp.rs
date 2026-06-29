5a23a246fb5180b352233d3f3b86e2cb610df352
Paweł Lidwin <lidwinpawel@gmail.com>
2026-06-29 13:21:58 +0200

    refactor: support parse file to accept generic reader

--- msixvc/src/xsp.rs
+++ msixvc/src/xsp.rs
@@ -1,28 +1,24 @@
 use crate::models::common::*;
 use crate::models::xsp::{XspHeader, XspPatchRecord};
-use tokio::{
-    fs::OpenOptions,
-    io::{AsyncSeekExt, BufReader},
-};
+use tokio::io::{AsyncRead, AsyncSeek};
+use tokio::io::{AsyncSeekExt, BufReader};
 
 pub struct XspFile {
     pub header: XspHeader,
     pub entries: Vec<XspPatchRecord>,
 }
 impl XspFile {
-    pub async fn parse_file(path: String) -> Result<Self, Box<dyn std::error::Error>> {
-        let file = OpenOptions::new()
-            .read(true)
-            .open(path.clone())
-            .await
-            .expect("Unable to open file");
-
+    pub async fn parse_file<Reader>(file: &mut Reader) -> Result<Self, Box<dyn std::error::Error>>
+    where
+        Reader: AsyncRead + AsyncSeek + Unpin,
+    {
         let mut file = BufReader::new(file);
 
         let header = read_struct!(XspHeader, file)?;
         let mut entries = Vec::with_capacity(header.record_count as usize);
         file.seek(std::io::SeekFrom::Start(header.page_size as u64))
             .await?;
+
         for _ in 0..header.record_count {
             let record = read_struct!(XspPatchRecord, file)?;
             entries.push(record);
